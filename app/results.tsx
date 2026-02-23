13ad837a9e5a1777d1eb6503b5d5766602baa8fb
concentratedmuesli <concentratedmuesli@gmail.com>
2026-02-23 13:42:45 +0100

    Make pass/fail pie chart

--- app/results.tsx
+++ app/results.tsx
@@ -1,9 +1,18 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
 
 export default function ShowResults() {
   const [items, setItems] = useState<any[]>([]);
   const [mergedItems, setMergedItems] = useState<any[]>([]);
+  const [score0, setScore0] = useState<any[]>([]);
+  const [score1, setScore1] = useState<any[]>([]);
+  const [score2, setScore2] = useState<any[]>([]);
+  const [score3, setScore3] = useState<any[]>([]);
+  const [noBechdelData, setNoBechdelData] = useState<any[]>([]);
+  const [chartData, setChartData] = useState<any>({});
+
   const [loading, setLoading] = useState(true);
 
   const { letterboxdHandle } = useParams<{ letterboxdHandle: string; }>();
@@ -63,24 +72,6 @@ export default function ShowResults() {
 
         setItems(items);
 
-        const allMovies = await import('./allMovies.json');
-        const allMoviesArray = allMovies.default;
-
-        const mergedItems = items.map(item => {
-          let matchingBechdelItem = allMoviesArray.find(movie => {
-            return movie.year === item.year && movie.title === item.title;
-          });
-
-          return {
-            ...item,
-            bechdelRating: matchingBechdelItem?.rating,
-            imdbId: matchingBechdelItem?.imdbid
-          };
-        });
-        
-        setMergedItems(mergedItems);
-        
-
       } catch (error) {
         console.error('Error:', error);
       } finally {
@@ -92,12 +83,78 @@ export default function ShowResults() {
 
   }, []);
 
+  useEffect(() => {
+    const getMatchingBechdelData = async () => {
+      const allMovies = await import('./allMovies.json');
+      const allMoviesArray = allMovies.default;
+
+      const mergedItems = items.map(item => {
+        let matchingBechdelItem = allMoviesArray.find(movie => {
+          return movie.year === item.year && movie.title === item.title;
+        });
+
+        return {
+          ...item,
+          bechdelRating: matchingBechdelItem?.rating,
+          imdbId: matchingBechdelItem?.imdbid
+        };
+      });
+      setMergedItems(mergedItems);
+    };
+    getMatchingBechdelData();
+  }, [items]);
+
+  useEffect(() => {
+    function createGraph() {
+      let score0 = mergedItems.filter(item => item.bechdelRating === 0);
+      let score1 = mergedItems.filter(item => item.bechdelRating === 1);
+      let score2 = mergedItems.filter(item => item.bechdelRating === 2);
+      let score3 = mergedItems.filter(item => item.bechdelRating === 3);
+      let noBechdelData = mergedItems.filter(item => !item.bechdelRating);
+
+      setScore0(score0);
+      setScore1(score1);
+      setScore2(score2);
+      setScore3(score3);
+      setNoBechdelData(noBechdelData);
+
+      ChartJS.register(ArcElement, Tooltip, Legend);
+
+      const data = {
+        labels: ['Pass', "Don't pass"],
+        datasets: [
+          {
+            
+            data: [score3.length, [score0.length, score1.length, score2.length].reduce((a, b) => a + b)],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+            ],
+            borderWidth: 1,
+          },
+        ],
+      };
+      setChartData(data);
+      console.log(data.datasets[0].data);
+    }
+    createGraph();
+
+  }, [items, mergedItems]);
 
   if (loading) return <div>Loading...</div>;
 
   return (
     <div>
-      {mergedItems.map((item, index) => (
+      <Doughnut data={chartData} className='max-w-50 max-h-100'/>
+      {/* {mergedItems.map((item, index) => (
         <div key={index} style={{ marginBottom: '20px', padding: '10px', border: '1px solid #ccc' }}>
           <h3>
             {item.title}
@@ -107,7 +164,7 @@ export default function ShowResults() {
           <p>Your rating: {item.rating}</p>
           <p>imdbid: {item.imdbId}</p>
         </div>
-      ))}
+      ))} */}
     </div>
   );
 }
