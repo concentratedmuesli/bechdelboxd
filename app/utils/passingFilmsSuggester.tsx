4410719550fcb63ea532ca18ad02070461d5dfe1
concentratedmuesli <concentratedmuesli@gmail.com>
2026-03-24 17:01:27 +0100

    Make sure there are no duplicates in the random movie suggestion

--- app/utils/passingFilmsSuggester.tsx
+++ app/utils/passingFilmsSuggester.tsx
@@ -1,14 +1,14 @@
 import type { bechdelItem, omdbAPIResponse } from '~/interfaces/items';
 import type { mergedBechdelItem } from '~/interfaces/items';
-import {escape, unescape} from 'html-escaper';
+import { escape, unescape } from 'html-escaper';
 
 
-const fetchPosterAndTitle = async (item: bechdelItem, key:string): Promise<{imageUrl:string, title:string}> => {
+const fetchPosterAndTitle = async (item: bechdelItem, key: string): Promise<{ imageUrl: string, title: string; }> => {
   const response = await fetch(`https://www.omdbapi.com/?i=tt${item.imdbid}&h=600&apikey=${key}`);
   const data: omdbAPIResponse = await response.json();
   const imageUrl = data.Poster;
   const title = data.Title;
-  return {imageUrl, title};
+  return { imageUrl, title };
 };
 
 export const GetRandomPassingFilms = async (): Promise<mergedBechdelItem[]> => {
@@ -18,13 +18,24 @@ export const GetRandomPassingFilms = async (): Promise<mergedBechdelItem[]> => {
     const allMovies = await import('../allMovies.json');
     const allMoviesArray = allMovies.default;
     const passingList = allMoviesArray.filter(item => item.rating === 3);
-    for (let i = 0; i < 20; i++) {
-      randomList.push(passingList[Math.floor(Math.random() * passingList.length)]);
+    
+    // Making sure there is no repetition in the suggestions
+    const usedIndices = new Set();
+    const maxCount = Math.min(20, allMoviesArray.length);
+
+    while (randomList.length < maxCount) {
+      const randomIndex = Math.floor(Math.random() * passingList.length);
+
+      if (!usedIndices.has(randomIndex)) {
+        usedIndices.add(randomIndex);
+        randomList.push(passingList[randomIndex]);
+      }
     }
+
     const mergedRandomList = await Promise.all(
       randomList.map(async (item: bechdelItem) => {
-        const {imageUrl, title} = await fetchPosterAndTitle(item, key);
-        // const correctedTitle = unescape(/,\s+The$/i.test(item.title) ? item.title.replace(/^(.+),\s+The$/i, 'The $1') : item.title);
+        const { imageUrl, title } = await fetchPosterAndTitle(item, key);
+
         return {
           ...item,
           title: title,
